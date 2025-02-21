166d3475b7ade580eaf5ef53c9d78d87645f3cf9
suichanwa <augustinnegura746@gmail.com>
2025-02-21 23:50:01 +0200

    added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// src/App.tsx
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
@@ -13,16 +15,19 @@ import CommunityManagePage from "./pages/Communities/CommunityManagePage";
 import Layout from "@/components/Layout";
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "./context/ThemeContext";
-import NotificationsPage from './pages/NotificationsPage';
+import NotificationsPage from "./pages/NotificationsPage";
 import PaymentPage from "./pages/PaymentPage";
-import ChatPage from './pages/ChatPage';
+import ChatPage from "./pages/ChatPage";
 import MangaDetailPage from "./pages/MangaDetailPage";
 import LightNovelDetailPage from "./pages/LightNovelDetailPage";
-import EditPoem from './pages/EditPoem';
+import EditPoem from "./pages/EditPoem";
 import BookDetailPage from "./pages/BookDetailPage";
-import BookReadPage from "@/pages/BookReadPage";
-import { PoemCommentsPage } from '@/pages/PoemCommentsPage';
-import PostDetailPage from "./pages/PostDetailPage"; 
+import BookReadPage from "./pages/BookReadPage";
+import { PoemCommentsPage } from "./pages/PoemCommentsPage";
+import { LoadingState } from "@/components/LoadingState";
+
+// Lazy load PostDetailPage (and any other heavy pages)
+const PostDetailPage = lazy(() => import("./pages/PostDetailPage"));
 
 function App() {
   return (
@@ -30,31 +35,33 @@ function App() {
       <ThemeProvider>
         <Router>
           <Layout>
-            <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/settings" element={<AccountSettingsPage />} />
-              <Route path="/bookmarks" element={<BookmarksPage />} />
-              <Route path="/setup-profile" element={<ProfileSetupPage />} />
-              <Route path="/poem/:id" element={<PoemDetail />} />
-              <Route path="/profile/:id" element={<ProfilePage />} />
-              <Route path="/communities" element={<CommunitiesPage />} />
-              <Route path="/communities/:id" element={<CommunityDetail />} />
-              <Route path="/communities/:id/manage" element={<CommunityManagePage />} />
-              <Route path="/notifications" element={<NotificationsPage />} />
-              <Route path="/payment" element={<PaymentPage />} />
-              <Route path="/chat/:chatId" element={<ChatPage />} /> {/* Added route for individual chat */}
-              <Route path="/chats" element={<ChatPage />} /> {/* Added route for chat list */}
-              <Route path="/manga/:id" element={<MangaDetailPage />} />
-              <Route path="/lightnovel/:id" element={<LightNovelDetailPage />} />
-              <Route path="/book/:id" element={<BookDetailPage />} />
-              <Route path="/edit-poem/:id" element={<EditPoem />} />
-              <Route path="/book/:id/read" element={<BookReadPage />} />
-              <Route path="/poems/:poemId/comments" element={<PoemCommentsPage />} />
-              <Route path="/post/:id" element={<PostDetailPage />} /> {/* Add the route for individual posts */}
-            </Routes>
+            <Suspense fallback={<LoadingState />}>
+              <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/settings" element={<AccountSettingsPage />} />
+                <Route path="/bookmarks" element={<BookmarksPage />} />
+                <Route path="/setup-profile" element={<ProfileSetupPage />} />
+                <Route path="/poem/:id" element={<PoemDetail />} />
+                <Route path="/profile/:id" element={<ProfilePage />} />
+                <Route path="/communities" element={<CommunitiesPage />} />
+                <Route path="/communities/:id" element={<CommunityDetail />} />
+                <Route path="/communities/:id/manage" element={<CommunityManagePage />} />
+                <Route path="/notifications" element={<NotificationsPage />} />
+                <Route path="/payment" element={<PaymentPage />} />
+                <Route path="/chat/:chatId" element={<ChatPage />} />
+                <Route path="/chats" element={<ChatPage />} />
+                <Route path="/manga/:id" element={<MangaDetailPage />} />
+                <Route path="/lightnovel/:id" element={<LightNovelDetailPage />} />
+                <Route path="/book/:id" element={<BookDetailPage />} />
+                <Route path="/edit-poem/:id" element={<EditPoem />} />
+                <Route path="/book/:id/read" element={<BookReadPage />} />
+                <Route path="/poems/:poemId/comments" element={<PoemCommentsPage />} />
+                <Route path="/post/:id" element={<PostDetailPage />} />
+              </Routes>
+            </Suspense>
           </Layout>
         </Router>
       </ThemeProvider>
